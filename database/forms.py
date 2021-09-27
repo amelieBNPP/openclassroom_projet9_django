33fad03cf4b5e69c223535ef9891cd41221546c3
amelieBNPP <amelie.noury@bnpparibas.com>
2021-09-27 18:09:51 +0200

    :feat: feature(form): add review form

--- database/forms.py
+++ database/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import models
 from django.forms import forms, CharField, ImageField, ModelForm
-from .models import Ticket, UserFollows
+from .models import Ticket, UserFollows, Review
 
 
 class CreateUserForm(UserCreationForm):
@@ -15,3 +15,11 @@ class CreateTicketForm(ModelForm):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'image']
+
+
+class CreateReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['headline', 'rating', 'body']
+        # widgets = {'rating': ModelForm.NumberInput(attrs={'class': 'Stars'})}
+        # labels = {'rating': 'Note /5'}
